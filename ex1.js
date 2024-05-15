7920a588be54cd362ace7d6d7b70e2ac8675b897
patchanaab <165882679+patchanaab@users.noreply.github.com>
2024-05-15 13:43:07 +0700

    exercise1

---
+++ ex1.js
@@ -0,0 +1,10 @@
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+function lengthArr(){
+    console.log(myAlphabet.length);
+}
+lengthArr();
+
+let Planets = ["planetA","planetB","planetC","planetD","planetE"]
+Planets.forEach((index,member) => {
+    console.log(index,member);
+});
